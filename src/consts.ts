f003b88605a1fdc25f0e91c770a6d0d9215f7891
Patrickill <1322301665@qq.com>
2024-02-06 19:31:04 +0800

    refactor: 博客内容重构

--- src/consts.ts
+++ src/consts.ts
@@ -4,27 +4,20 @@
  * A few of these are not used yet, and are subject to change, example of this is Author.
  */
 export const SITE_METADATA = {
-    title: 'Astro Starter Blog',
-    headerTitle: 'Astro Starter Blog',
-    description: 'A blog created with Astro and Tailwind.css',
-    language: 'en-us',
+    title: 'papapatrick的妙妙屋',
+    headerTitle: 'papapatrcik’s Blog',
+    description: '一名在校大二软工学生的奇思妙想',
+    language: 'zh-CN',
     theme: 'system', // Options: system, light, dark, Does not work yet
     siteUrl: 'https://astro-starter-blog.yon.fun/',
-    siteRepo: 'https://github.com/wanoo21/tailwind-astro-starting-blog',
+    siteRepo: 'https://github.com/Patrickill/my-blog',
     siteLogo: '/static/images/logo.png',
-    socialBanner: '/static/images/twitter-card.png',
-    locale: 'en-US',
+    locale: 'zh-CN',
 
     // The following are subject to change. They are placeholders for now.
     // This project provide a default author content see src/content/authors/default.mdx, so these details are better to be inserted there.
-    author: 'John Doe',
-    mastodon: 'https://mastodon.social/@mastodonuser',
-    email: 'address@yoursite.com',
-    github: 'https://github.com',
-    twitter: 'https://twitter.com/Twitter',
-    facebook: 'https://facebook.com',
-    youtube: 'https://youtube.com',
-    linkedin: 'https://www.linkedin.com',
+    author: 'papapatrick',
+    github: 'https://github.com/Patrickill',
 
     // These are not supported yet
     // analytics: {
@@ -106,6 +99,6 @@ export const NAVIGATION = [
     {href: '/', title: 'Home'},
     {href: '/blog', title: 'Blog'},
     {href: '/tags', title: 'Tags'},
-    {href: '/projects', title: 'Projects'},
+    // {href: '/projects', title: 'Projects'},
     {href: '/about', title: 'About'},
 ]
